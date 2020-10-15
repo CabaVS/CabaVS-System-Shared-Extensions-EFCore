71c09046aca7db190810e2b0158c5a8e2d134d75
Valerii <valerii.savchenko@icloud.com>
2020-10-15 13:40:35 +0300

    Initial commit; Added IQueryable extensions

---
+++ QueryableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace CabaVS.Shared.Extensions.EFCore
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> ApplyInclude<T>(this IQueryable<T> expression, string[] includes, bool isOptional = true) where T : class
+        {
+            if (expression == null) throw new ArgumentNullException(nameof(expression));
+            if (!isOptional && includes == null) throw new ArgumentNullException(nameof(includes));
+
+            return includes != null && includes.Length > 0
+                ? includes.Aggregate(expression,
+                    (current, navigationPropertyPath) => current.Include(navigationPropertyPath))
+                : expression;
+        }
+    }
+}
